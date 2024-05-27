c85aee2e7e928902528b3887a4ada81233561bb9
Menaka Godakanda <156875412+MenakaGodakanda@users.noreply.github.com>
2024-05-27 13:33:20 +0800

    Update Add-Users.ps1

--- scripts/Add-Users.ps1
+++ scripts/Add-Users.ps1
@@ -1,1 +1,19 @@
+# Add users to AD
+foreach ($user in $users) {
+    $name = "$($user.FirstName) $($user.LastName)"
+    $username = $user.Username
+    $password = (ConvertTo-SecureString $user.Password -AsPlainText -Force)
+    $userPrincipalName = "$username@mydomain.local"
 
+    New-ADUser -Name $name `
+               -GivenName $user.FirstName `
+               -Surname $user.LastName `
+               -SamAccountName $username `
+               -UserPrincipalName $userPrincipalName `
+               -AccountPassword $password `
+               -Enabled $true `
+               -PasswordNeverExpires $true `
+               -Path "CN=Users,DC=mydomain,DC=local"
+
+    Write-Output "Created user: $name"
+}
